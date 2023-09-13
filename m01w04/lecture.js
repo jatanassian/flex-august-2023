1c4e4b8ed35ee713e57d2e17f1de0f1528dc1d6e
Julien Atanassian <julien.atanassian@gmail.com>
2023-09-12 22:48:01 -0400

    update comments

--- m01w04/lecture.js
+++ m01w04/lecture.js
@@ -1,29 +1,32 @@
 // Asynchronous Flow
 
 // Synchronous
-// const bob = 'Robert';
-// console.log(`Hi ${bob}`);
+const bob = 'Robert';
+console.log(`Hi ${bob}`);
 
-// console.log('Yo!');
+// Example of asynchronous
+// Output: Yo > Goodbye > What's up > Not much
+console.log('Yo!');
 
-// setTimeout(() => {
-// 	console.log("Hey what's up?");
-// }, 5000);
+setTimeout(() => {
+	console.log("Hey what's up?");
+}, 5000);
 
-// setTimeout(() => {
-// 	console.log('Not much, you?');
-// }, 5000);
+setTimeout(() => {
+	console.log('Not much, you?');
+}, 5000);
 
-// console.log('Goodbye');
+console.log('Goodbye');
 
-// const word = 'HELLOOO';
-// for (let i = 0; i < word.length; i++) {
-// 	const letter = word[i];
+// Exercise: print letters one by one with a 1 second delay between each letter
+const word = 'HELLOOO';
+for (let i = 0; i < word.length; i++) {
+	const letter = word[i];
 
-// 	setTimeout(() => {
-// 		console.log(letter);
-// 	}, 1000 * (i + 1));
-// }
+	setTimeout(() => {
+		console.log(letter);
+	}, 1000 * (i + 1)); // If we don't increase the delay, all the letters would appear after 1 second
+}
 
 // Add water to the pot, put pot on stove on max, wait 10.5min => BAD
 // Add water to the pot, put pot on stove on max, wait until boiling => GOOD
@@ -35,7 +38,7 @@
 
 // Traps to pay attention to:
 
-const errorProbably = name => {
+const errorProbably = () => {
 	console.log(name);
 };
 
@@ -45,4 +48,4 @@ const dangerFunction = () => {
 	errorProbably(name);
 };
 
-dangerFunction();
+// Make sure to pass name as a variable
